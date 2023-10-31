519473e84868e3be09b85ec121a49f1b0ba591ae
Vitor Stahlberg <vitorlberg@gmail.com>
2023-10-31 09:57:06 -0300

    Add testes\java

---
+++ codigo/src/tests/java/Teste.java
@@ -0,0 +1,11 @@
+package tests.java;
+import org.junit.Test;
+
+import main.java.models.Usuario;
+
+public class Teste {
+    @Test
+    public void test(){
+        Usuario tes = new Usuario();
+    }
+}
