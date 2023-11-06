b5930b275bf62a473c3a41cb55fcf1fb961603c2
duda-amaral <dudaamaralmap3110@gmail.com>
2023-11-06 14:46:47 -0300

    code: add escolherStatusEmprestimo() to ItemInput

--- codigo/src/main/java/views/inputs/ItemInput.java
+++ codigo/src/main/java/views/inputs/ItemInput.java
@@ -1,9 +1,11 @@
 package main.java.views.inputs;
 
 import main.java.enums.StatusClassificacao;
+import main.java.enums.StatusEmprestimo;
 import main.java.utils.InputScannerUtil;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class ItemInput {
 
@@ -24,6 +26,23 @@ public class ItemInput {
             return null;
         }
 
+    }
+
+    protected static StatusEmprestimo escolherStatusEmprestimo() {
+
+        for (StatusEmprestimo status : StatusEmprestimo.values()) {
+            System.out.println(status.ordinal() + 1 + ". " + status.name());
+        }
+
+        int escolha = InputScannerUtil.scanner.nextInt();
+        InputScannerUtil.scanner.nextLine();
+
+        if (escolha >= 1 && escolha <= StatusEmprestimo.values().length) {
+            return StatusEmprestimo.values()[escolha - 1];
+        } else {
+            System.out.println("Opção inválida.");
+            return null;
+        }
     }
 
     protected static void inserirConteudo(List<String> conteudo) {
